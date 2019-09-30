7ad5f86d605556035fbb10b752591e72ee43edb1
sunqi <sunq@miliantech.comi>
2019-09-30 18:23:32 +0800

    函数部分学习

--- test_1/36_Closure.go
+++ test_1/36_Closure.go
@@ -11,3 +11,11 @@ func closureTest() {
     tom := f()
     jerry := f()
 }
+
+func Adder() func(int) int {
+    var x int
+    return func(delta int) int {
+        x += delta
+        return x
+    }
+}
